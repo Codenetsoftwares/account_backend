a7e2d9abf44c86d93a2df920b26f9f8ae2dfabb5
abusaad17 <abusaadgulzar1@gmail.com>
2023-07-10 13:17:37 +0530

    updated

--- Routes/Transaction.route.js
+++ Routes/Transaction.route.js
@@ -80,42 +80,42 @@ const TransactionRoutes = (app) => {
     }
   );
 
-   /**
-   * @swagger
-   * /deposit/transaction:
-   *   post:
-   *     tags: [Transaction]
-   *     summary: Entry of Deposit
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema:
-   *             properties:
-   *              transactionID:
-   *                type: string
-   *                description: The Transaction ID  of the transaction
-   *                example: 45498421246435
-   *              transactionType:
-   *                type: string
-   *                description: The transactionType of the transaction
-   *                example: Deposit/withdraw
-   *              withdrawAmount:
-   *                type: Number
-   *                description: The withdrawAmount of the transaction
-   *                example: true
-   *              status:
-   *                type: boolean
-   *                description: Status of the transaction
-   *                example: true
-   *     responses:
-   *       200:
-   *        description: The user was logged in successfully
-   *       400:
-   *        description: Bad Request
-   *       500:
-   *        description: Internal Server Error
-   */
+  /**
+  * @swagger
+  * /deposit/transaction:
+  *   post:
+  *     tags: [Transaction]
+  *     summary: Entry of Deposit
+  *     requestBody:
+  *       required: true
+  *       content:
+  *         application/json:
+  *           schema:
+  *             properties:
+  *              transactionID:
+  *                type: string
+  *                description: The Transaction ID  of the transaction
+  *                example: 45498421246435
+  *              transactionType:
+  *                type: string
+  *                description: The transactionType of the transaction
+  *                example: Deposit/withdraw
+  *              withdrawAmount:
+  *                type: Number
+  *                description: The withdrawAmount of the transaction
+  *                example: true
+  *              status:
+  *                type: boolean
+  *                description: Status of the transaction
+  *                example: true
+  *     responses:
+  *       200:
+  *        description: The user was logged in successfully
+  *       400:
+  *        description: Bad Request
+  *       500:
+  *        description: Internal Server Error
+  */
 
   app.post(
     '/deposit/transaction',
@@ -128,7 +128,7 @@ const TransactionRoutes = (app) => {
       }
     }
   );
-  
+
   /**
    * @swagger
    * /api/deposit/view:
@@ -171,11 +171,11 @@ const TransactionRoutes = (app) => {
   app.post("/api/deposit/filter-dates", authenticateToken("admin"), async (req, res) => {
     try {
       const { startDate, endDate } = req.body;
-  
+
       const start = new Date(startDate);
       const end = new Date(endDate);
       end.setDate(end.getDate() + 1);
-  
+
       const depositView = await Transaction.find({
         transactionType: "deposit",
         createdAt: { $gte: start, $lt: end }
@@ -194,11 +194,11 @@ const TransactionRoutes = (app) => {
   app.post("/api/withdraw/filter-dates", authenticateToken("admin"), async (req, res) => {
     try {
       const { startDate, endDate } = req.body;
-  
+
       const start = new Date(startDate);
       const end = new Date(endDate);
       end.setDate(end.getDate() + 1);
-  
+
       const withdrawView = await Transaction.find({
         transactionType: "withdraw",
         createdAt: { $gte: start, $lt: end }
@@ -213,7 +213,28 @@ const TransactionRoutes = (app) => {
       res.status(e.code || 500).send({ message: e.message || "Internal server error" });
     }
   });
-  
+
+  app.post("/api/admin/edit-transaction/:id", authenticateToken("admin"), async (req, res) => {
+    try {
+      const trans = await Transaction.findById(req.params.id);
+      const { amount, id, paymentMethod } = req.body;
+      if (amount) {
+        trans.transactionType === "withdraw" ? trans.withdrawAmount = amount : trans.depositAmount = amount;
+      }
+      if (id) {
+        trans.transactionID = id;
+      }
+      if (paymentMethod) {
+        trans.paymentMethod = paymentMethod;
+      }
+      trans.save();
+      res.status(200).send({ message: "transaction edited" });
+    } catch (e) {
+      console.error(e);
+      res.status(e.code || 500).send({ message: e.message || "Internal server error" });
+    }
+  });
+
 };
 
 export default TransactionRoutes;
