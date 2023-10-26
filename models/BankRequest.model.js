ba753aad6203ded8b7e1dc32a385b593fd178d2b
akmworld <98544580+akmworld@users.noreply.github.com>
2023-10-26 16:18:38 +0530

    updated code

---
+++ models/BankRequest.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export const BankRequest = mongoose.model(
+  "BankRequest",
+  new mongoose.Schema({
+    bankName: { type: String, required: true },
+    accountHolderName: { type: String },
+    accountNumber: { type: Number },
+    ifscCode: { type: String },
+    upiId: { type: String },
+    upiAppName: { type: String },
+    upiNumber: { type: String },
+    subAdminId: [],
+    subAdminName: { type: String },
+    createdAt: { type: Date },
+    isApproved: { type: Boolean, default: false },
+    isActive: {type: Boolean, default: false, required: true}
+  }),
+  "BankRequest"
+);
